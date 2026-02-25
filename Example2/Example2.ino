5942ef19f054f28750ab6bab7b763c128f6fa32a
Francois ND <francoisnd1@gmail.com>
2026-02-25 13:54:24 +0100

    Add BME280 I2C sensor reading to Example2

--- Example2/Example2.ino
+++ Example2/Example2.ino
@@ -1,11 +1,19 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
 
 // ===== WIFI AP =====
 const char* ssid = "LINKY-TIC";
 const char* password = "12345678";
 
 ESP8266WebServer server(80);
+
+// ===== BME280 =====
+Adafruit_BME280 bme;
+bool bmeOk = false;
+float envTemp = 0, envHum = 0, envPres = 0;
 
 String bufferTIC = "";
 
@@ -24,9 +32,13 @@ void handleRoot() {
 }
 
 void setup() {
-
   // ===== TIC Linky =====
   Serial.begin(1200, SERIAL_7E1);   // RX GPIO3
+
+  // ===== BME280 =====
+  Wire.begin();
+  bmeOk = bme.begin(0x76);
+  if (!bmeOk) bmeOk = bme.begin(0x77);
 
   // ===== WIFI AP =====
   WiFi.mode(WIFI_AP);
@@ -39,17 +51,21 @@ void setup() {
 }
 
 void loop() {
-
   server.handleClient();
+
+  // Lecture BME280 toutes les 10s
+  static unsigned long lastBME = 0;
+  if (bmeOk && millis() - lastBME > 10000) {
+    envTemp = bme.readTemperature();
+    envHum  = bme.readHumidity();
+    envPres = bme.readPressure() / 100.0;
+    lastBME = millis();
+  }
 
   // Lecture TIC
   while (Serial.available()) {
-
     char c = Serial.read();
-
     bufferTIC += c;
-
-    // limite mémoire (important ESP8266)
     if (bufferTIC.length() > 4000) {
       bufferTIC.remove(0, 2000);
     }
